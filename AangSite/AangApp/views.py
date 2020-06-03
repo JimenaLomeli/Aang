60a07d69f561a080053ea3456d7496965b33a240
JimenaLomeli <jimena@lomeli.mx>
2020-06-03 00:18:30 -0500

    Added canvas and color render

--- AangSite/AangApp/views.py
+++ AangSite/AangApp/views.py
@@ -9,6 +9,7 @@ from subprocess import call
 
 @csrf_exempt
 def index(request):
+    context = {}
     if request.method == 'POST':
         form = codigo(request.POST)
         if form.is_valid():
@@ -20,10 +21,21 @@ def index(request):
             inp = "./inputFile.txt"
             call(["Python3", "/Users/jime/Desktop/Aang/Grammar/AangMain.py", "/Users/jime/Desktop/Aang/AangSite/input.txt"])
             call(["Python3", "/Users/jime/Desktop/Aang/Grammar/VirtualMachine.py"])
+            context = {           
+                'color1' : 'red',
+                'color2' : 'blue',
+                'color3' : 'red',
+                'color4' : 'green'
+            }
+
 
     form = codigo()
+    context['codigo'] = form
 
-    return render(request, 'index.html', {'codigo': form})
+
+
+
+    return render(request, 'index.html', context = context)
 
 
 
